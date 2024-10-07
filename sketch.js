f329b07a597a77853b4731a3ada77ae0cadd611d
Ayymer <82091839+Ayymer@users.noreply.github.com>
2024-10-07 14:24:52 +0200

    Start

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(800, 800);
+  }
+  
+  function draw() {
+    background(220);
+    // cercle
+    fill(255, 0, 0);
+    if(mouseIsPressed != true){
+      ellipse(400, 300, 50);
+    }
+  
+  }
