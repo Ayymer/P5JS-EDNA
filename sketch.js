9a806b073bd74db432bb795ce40fa57390a9268a
Ayymer <82091839+Ayymer@users.noreply.github.com>
2024-10-07 14:57:01 +0200

    update key function

--- sketch.js
+++ sketch.js
@@ -1,13 +1,37 @@
+//var size = 100;
+
+let circle = {
+  x: 400,
+  y: 300,
+  size: 50,
+  color: [255, 0, 0],
+};
+
 function setup() {
-    createCanvas(800, 800);
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+  // cercle
+  fill(255, 0, 0);
+  if (mouseIsPressed != true) {
+    ellipse(circle.x, circle.y, circle.size);
   }
-  
-  function draw() {
-    background(220);
-    // cercle
-    fill(255, 0, 0);
-    if(mouseIsPressed != true){
-      ellipse(400, 300, 50);
-    }
-  
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    circle.y -= 10;
   }
+  if (keyCode == DOWN_ARROW) {
+    circle.y += 10;
+  }
+  if (keyCode == LEFT_ARROW) {
+    circle.x -= 10;
+  }
+  if (keyCode == RIGHT_ARROW) {
+    circle.x += 10;
+  }
+}
+
