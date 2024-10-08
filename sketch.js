ba8fc84fb6ada48cd8d5ba307b8a5f5ded3481d7
Ayymer <82091839+Ayymer@users.noreply.github.com>
2024-10-08 13:47:25 +0200

    update

--- sketch.js
+++ sketch.js
@@ -2,19 +2,46 @@ class Circle {
   constructor(startX, startY) {
     this.x = startX;
     this.y = startY;
-    this.size = 50;
+    this.size = 5;
     this.color = [random(255), random(255), random(255)];
+    this.maxSize = random(50, 150);
+  }
+  affichercercle() {
+    fill(this.color);
+    ellipse(this.x, this.y, this.size);
   }
 
-  afficher() {
+  grandircercle() {
+    if (this.size< this.maxSize) {
+      this.size += 1;
+    }
+  }
+
+}
+
+class Square {
+  constructor(startX, startY) {
+    this.x = startX + 5;
+    this.y = startY + 5;
+    this.size = 2;
+    this.color = [random(255), random(255), random(255)];
+    this.maxSize = random(50, 150);
+  }
+
+  affichercarre() {
     fill(this.color);
     square(this.x, this.y, this.size);
-    ellipse(this.x, this.y, this.size);
-    
+  }
+
+  grandircarre() {
+    if (this.size< this.maxSize) {
+      this.size += 1;
+    }
   }
 }
 
 let circleList = [];
+let squareList = [];
 
 function setup() {
   createCanvas(735, 880);
@@ -23,26 +50,18 @@ function setup() {
 function draw() {
   background(220);
 
-  for(let circle of circleList) {
-    circle.afficher();
+  for (let circle of circleList) {
+    circle.affichercercle();
+    circle.grandircercle();
+  }
+
+  for (let square of squareList) {
+    square.affichercarre();
+    square.grandircarre();
   }
 }
 
 function mousePressed() {
   circleList.push(new Circle(mouseX, mouseY));
+  squareList.push(new Square(mouseX, mouseY));
 }
-
-// function keyPressed() {
-//   if (keyCode == UP_ARROW) {
-//     this.y -= 10;
-//   }
-//   if (keyCode == DOWN_ARROW) {
-//     this.y += 10;
-//   }
-//   if (keyCode == LEFT_ARROW) {
-//     this.x -= 10;
-//   }
-//   if (keyCode == RIGHT_ARROW) {
-//     this.x += 10;
-//   }
-// }
